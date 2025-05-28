dcbb7e95c45196fdda19d5414f6290e0a6b4add1
Tiyasha Dutta <d22.tiyasha@gmail.com>
2025-05-28 20:14:29 +0100

    added update and delete route

--- BookStoreApp/server/routes/booksRoutes.js
+++ BookStoreApp/server/routes/booksRoutes.js
@@ -15,6 +15,7 @@ router.post("/add", async (req, res) => {
     }
 })
 
+//get all books
 router.get("/getBooks", async (req, res) => {
     let books;
     try {
@@ -22,6 +23,45 @@ router.get("/getBooks", async (req, res) => {
         res.status(200).json({ books });
     } catch (error) {
         console.log(error);
+    }
+});
+
+//get books by their _id
+router.get("/getBooks/:id", async (req, res) => {
+    let books;
+    const id = req.params.id;
+    try {
+       books = await bookModel.findById(id);
+       res.status(200).json({ books });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//update book by id
+router.put("/updateBook/:id", async (req, res) => {
+    let book;
+    const id = req.params.id;
+    const { bookname, description, author, image, price } = req.body;
+    try {
+        book = await bookModel.findByIdAndUpdate(id, { bookname, description, author, image, price });
+        await book.save().then(() => 
+            res.status(200).json({message: "Book updated successfully"}));
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//Delete book by id
+router.delete("/deleteBook/:id", async (req, res) => {
+    let book;
+    const id = req.params.id;
+    try {
+        book = await bookModel.findByIdAndDelete(id).then(() =>{
+            res.status(200).json({message: "book deleted successfully"});
+        })
+    } catch (error) {
+        console.log(error);                
     }
 })
 
