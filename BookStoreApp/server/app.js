b8b273784cade4808d5e6a023b36c0f41f08b565
Tiyasha Dutta <d22.tiyasha@gmail.com>
2025-05-28 19:28:45 +0100

    Added get request for the routes

--- BookStoreApp/server/app.js
+++ BookStoreApp/server/app.js
@@ -7,6 +7,8 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.use("/api/routes", bookRoutes)
 
 app.listen (1000, () => {
--- BookStoreApp/server/routes/booksRoutes.js
+++ BookStoreApp/server/routes/booksRoutes.js
@@ -6,10 +6,20 @@ router.post("/add", async (req, res) => {
     try {
         const data = req.body;
         const newBook = new bookModel(data);
-        await newBook.save().then((res) => {
+        await newBook.save().then( () => {
             res.status(200).json({message: "Book added successfully"});
         })
 
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get("/getBooks", async (req, res) => {
+    let books;
+    try {
+        books = await bookModel.find();
+        res.status(200).json({ books });
     } catch (error) {
         console.log(error);
     }
